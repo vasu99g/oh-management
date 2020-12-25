921e2f774bc70c66bce7fc8c587dc80ca1495d02
Vasu Gupta <vasu99g@uw.edu>
2020-12-26 01:08:49 +0530

    from, input-field i, btn transition not working. Database up and create a class up.

--- app.py
+++ app.py
@@ -1,12 +1,50 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.secret_key = "key"
+
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///static/oh-management.sqlite3"
+
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def index():
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    email = db.Column(db.String(100))
+    zoom_id = db.Column(db.String(15))
+
+    def __init__(self, name, email, zoom_id):
+        self.name = name
+        self.email = email
+        self.zoom_id = zoom_id
+
+
+@app.route("/")
+def login():
     return render_template("login.html")
 
 
-if __name__ == '__main__':
+@app.route("/student_dashboard/<student_id>")
+def student_dashboard(student_id):
+    return render_template("student_dashboard.html")
+
+
+@app.route("/student_login", methods=["POST"])
+def student_login():
+    if request.method == "POST":
+        name = request.form["name"]
+        email = request.form["email"]
+        zoom_id = request.form["zoom_id"]
+
+        student = Student(name, email, zoom_id)
+        db.session.add(student)
+        db.session.commit()
+
+        return redirect(url_for("student_dashboard", student_id=student.id))
+
+
+if __name__ == "__main__":
     app.run()
--- static/login.js
+++ static/login.js
@@ -1,11 +1,27 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
+const class_form_btn = document.querySelector("#class-form-btn");
 const container = document.querySelector(".container");
+let class_form_mode = false;
 
 sign_up_btn.addEventListener("click", () => {
-  container.classList.add("sign-up-mode");
+    container.classList.add("sign-up-mode");
 });
 
 sign_in_btn.addEventListener("click", () => {
-  container.classList.remove("sign-up-mode");
+    container.classList.remove("sign-up-mode");
+});
+
+class_form_btn.addEventListener("click", () => {
+    if (class_form_mode) {
+        document.getElementById("instructor-form").style.visibility = "visible";
+        document.getElementById("class-form").style.visibility = "hidden";
+        class_form_btn.innerHTML = "Create a Class";
+        class_form_mode = false;
+    } else {
+        document.getElementById("instructor-form").style.visibility = "hidden";
+        document.getElementById("class-form").style.visibility = "visible";
+        class_form_btn.innerHTML = "Instructor Login";
+        class_form_mode = true;
+    }
 });
