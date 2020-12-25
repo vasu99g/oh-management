78ceed944846d30a7780e7d920a2bd1969c285f3
Vasu Gupta <vasu99g@uw.edu>
2020-12-26 01:52:02 +0530

    Fixed form transition, and student login button sizing.

--- static/login.js
+++ static/login.js
@@ -1,26 +1,38 @@
 const sign_in_btn = document.querySelector("#sign-in-btn");
 const sign_up_btn = document.querySelector("#sign-up-btn");
-const class_form_btn = document.querySelector("#class-form-btn");
 const container = document.querySelector(".container");
+
+const class_form_btn = document.getElementById("class-form-btn");
+const instructor_form = document.getElementById("instructor-form");
+const class_form = document.getElementById("class-form");
+
 let class_form_mode = false;
 
 sign_up_btn.addEventListener("click", () => {
+    // instructor login page
     container.classList.add("sign-up-mode");
 });
 
 sign_in_btn.addEventListener("click", () => {
+    // student login page
+    instructor_form.style.transition = "all 0.2s 0.7s";
+    class_form.style.transition = "all 0.2s 0.7s";
+
     container.classList.remove("sign-up-mode");
 });
 
 class_form_btn.addEventListener("click", () => {
+    instructor_form.style.transition = "0s";
+    class_form.style.transition = "0s";
+
     if (class_form_mode) {
-        document.getElementById("instructor-form").style.visibility = "visible";
-        document.getElementById("class-form").style.visibility = "hidden";
+        instructor_form.style.visibility = "visible";
+        class_form.style.visibility = "hidden";
         class_form_btn.innerHTML = "Create a Class";
         class_form_mode = false;
     } else {
-        document.getElementById("instructor-form").style.visibility = "hidden";
-        document.getElementById("class-form").style.visibility = "visible";
+        instructor_form.style.visibility = "hidden";
+        class_form.style.visibility = "visible";
         class_form_btn.innerHTML = "Instructor Login";
         class_form_mode = true;
     }
